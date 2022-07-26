e35fb80a4f02874f634887e5663a07855c2b27a3
JacobM311 <jacobmarshall9608@yahoo.com>
2022-07-26 10:32:33 -0700

    fixed getenv function

--- getenv.c
+++ getenv.c
@@ -8,14 +8,8 @@ char *_getenv(const char *var)
 	{
 		match = strncmp(environ[i], var, len);
 		if (match == 0)
-			return (environ[i + len + 1]);
+			return (environ[i] + len + 1);
 		i++;
 	}
 	return (NULL);
-}
-
-int main(void)
-{
-	printf("%s\n", _getenv("PATH"));
-	return (0);
 }
